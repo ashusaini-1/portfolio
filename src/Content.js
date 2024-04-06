883fe3a2d201a479f77bc08dc15d75e3bc110f3b
ashu <ashusaini17354@gmail.com>
2024-04-06 20:54:28 +0530

    commited

--- src/Content.js
+++ src/Content.js
@@ -13,8 +13,8 @@ import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
 // import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
+// import project2 from "./assets/images/projects/img2.png";
+// import project3 from "./assets/images/projects/img3.png";
 import person_project from "./assets/images/projects/person.png";
 
 import avatar1 from "./assets/images/Testimonials/avatar1.png";
@@ -135,19 +135,19 @@ export const content = {
   Projects: {
     title: "Projects",
     subtitle: "MY CREATION",
-    image: person_project,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzO-mVbTLfydeqdGNHIui3JqxaitpjPKIZw&s",
     project_content: [
       {
         title: "Lorem Ipsum",
-        image: project2,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzO-mVbTLfydeqdGNHIui3JqxaitpjPKIZw&s",
       },
       {
         title: "Lorem Ipsum",
-        image: project2,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzO-mVbTLfydeqdGNHIui3JqxaitpjPKIZw&s",
       },
       {
         title: "Lorem Ipsum",
-        image: project2,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzO-mVbTLfydeqdGNHIui3JqxaitpjPKIZw&s",
       },
     ],
   },
